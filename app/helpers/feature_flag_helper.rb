2cbfbdac9c0566e8ff4ae36cdf4f5f16d290dc78
Hannu Lyytikainen <hannu@sharetribe.com>
2016-08-08 09:41:49 +0300

    Refactor feature flag helper
    Allow enabled-for-user and enabled-for-community kind of queries.

--- app/helpers/feature_flag_helper.rb
+++ app/helpers/feature_flag_helper.rb
@@ -8,11 +8,26 @@ module FeatureFlagHelper
   module_function
 
   def init(request, is_admin)
-    RequestStore.store[:feature_flags] ||= fetch_feature_flags(request, is_admin)
+    community_flags = fetch_community_flags(request)
+    person_flags = fetch_person_flags(request)
+    temp_flags = fetch_temp_flags(is_admin, request.params, request.session)
+    request.session[:feature_flags] = temp_flags
+
+    RequestStore.store[:person_feature_flags] ||= person_flags
+    RequestStore.store[:community_feature_flags] ||= community_flags
+    RequestStore.store[:feature_flags] ||= community_flags.union(person_flags).union(temp_flags)
   end
 
   def feature_enabled?(feature_name)
     feature_flags.include? feature_name
+  end
+
+  def feature_enabled_for_community?(feature_name)
+    community_feature_flags.include? feature_name
+  end
+
+  def feature_enabled_for_user?(feature_name)
+    person_feature_flags.include? feature_name
   end
 
   def assert_feature_enabled(feature_name)
@@ -23,6 +38,20 @@ module FeatureFlagHelper
     block.call if feature_enabled?(feature_name)
   end
 
+  def person_feature_flags
+    unless RequestStore.store[:person_feature_flags]
+      raise FeatureFlagHelperNotInitialized.new("Feature flags helper not initialized! Call 'init' first.")
+    end
+    RequestStore.store[:person_feature_flags]
+  end
+
+  def community_feature_flags
+    unless RequestStore.store[:community_feature_flags]
+      raise FeatureFlagHelperNotInitialized.new("Feature flags helper not initialized! Call 'init' first.")
+    end
+    RequestStore.store[:community_feature_flags]
+  end
+
   def feature_flags
     unless RequestStore.store[:feature_flags]
       raise FeatureFlagHelperNotInitialized.new("Feature flags helper not initialized! Call 'init' first.")
@@ -30,18 +59,16 @@ module FeatureFlagHelper
     RequestStore.store[:feature_flags]
   end
 
-  def fetch_feature_flags(request, is_admin)
-    community_flags_from_service = FeatureFlagService::API::Api.features.get(community_id: community_id(request)).maybe[:features].or_else(Set.new)
+  # Fetch community-specific flags from the feature flag service
+  def fetch_community_flags(request)
+    FeatureFlagService::API::Api.features.get(community_id: community_id(request)).maybe[:features].or_else(Set.new)
+  end
 
-    person_flags_from_service = FeatureFlagService::API::Api.features
+  # Fetch person-specific flags from the feature flag service
+  def fetch_person_flags(request)
+    FeatureFlagService::API::Api.features
       .get(community_id: community_id(request), person_id: person_id(request))
       .maybe[:features].or_else(Set.new)
-
-    temp_flags = fetch_temp_flags(is_admin, request.params, request.session)
-
-    request.session[:feature_flags] = temp_flags
-
-    community_flags_from_service.union(person_flags_from_service).union(temp_flags)
   end
 
   # Fetch temporary flags from params and session
